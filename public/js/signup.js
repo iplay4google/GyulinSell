c2ba30e41d28458483b60ecb3fa52a1d9c9ea5b5
Tunahan <you@example.com>
2026-03-13 20:31:03 +0200

    Fixed signup system

---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("signupForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    })
+  });
+
+  const data = await res.json();
+
+  if(data.error){
+    alert(data.error);
+    return;
+  }
+
+  alert("Account created!");
+  window.location.href = "/login.html";
+});
